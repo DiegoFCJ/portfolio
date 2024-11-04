e176872bff96014aec6591a52755c0113308ed8c
diegoeffe96@gmail.com <diegoeffe96@gmail.com>
2024-11-04 22:45:37 +0100

    adding first home page and projects overview

---
+++ src/app/components/projects/projects.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Project } from '../../dtos/ProjectDTO';
+
+@Component({
+  selector: 'app-projects',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './projects.component.html',
+  styleUrl: './projects.component.scss'
+})
+export class ProjectsComponent {
+  projects: Project[] = [
+    {
+      title: 'Micro Games',
+      description: 'A collection of simple games built using various technologies. Key Features: Multiple mini-games designed to challenge players. Interactive UI with responsive design. Easy to extend with new games and features.',
+      image: 'https://github.com/DiegoFCJ/MicroGames/blob/master/overview/loggedPage.png?raw=true',
+      link: 'https://github.com/DiegoFCJ/MicroGames'
+    },
+    {
+      title: 'Self',
+      description: 'This project provides tools for tracking goals and progress, focusing on self-improvement and productivity through an intuitive interface. Users can add plugins for enhanced functionality. Key Features: Goal tracking and visualization User-friendly task management dashboard Optimized with modern web technologies',
+      image: 'https://github.com/DiegoFCJ/self/blob/master/self.png?raw=true',
+      link: 'https://github.com/DiegoFCJ/self'
+    },
+    {
+      title: 'E-commerce',
+      description: 'This repository serves as the foundation for an e-commerce application, providing essential features and structure for building a robust online shopping platform.',
+      image: 'https://github.com/DiegoFCJ/E-commerce/blob/master/overview/homeSideDash.png?raw=true',
+      link: 'https://github.com/DiegoFCJ/E-commerce'
+    }
+  ];
+}
