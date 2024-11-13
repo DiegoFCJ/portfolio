98bd76a0365bf5b4a9acfba5f07e6490c5ccb50d
diegoeffe96@gmail.com <diegoeffe96@gmail.com>
2024-11-13 14:47:48 +0100

    base href change and adding social

---
+++ src/app/data/socials.data.ts
@@ -0,0 +1,19 @@
+import { Social } from '../dtos/SocialDTO';
+
+export const projects: Social[] = [
+    {
+        title: 'Linkedin',
+        link: 'https://github.com/DiegoFCJ/MicroGames',
+        icon: 'fas fa-gamepad'
+    },
+    {
+        title: 'Discord',
+        link: 'https://github.com/DiegoFCJ/self',
+        icon: 'fas fa-gamepad'
+    },
+    {
+        title: 'Instagram',
+        link: 'https://github.com/DiegoFCJ/E-commerce',
+        icon: 'fas fa-gamepad'
+    }
+];
