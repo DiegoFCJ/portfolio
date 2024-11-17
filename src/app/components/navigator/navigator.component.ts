a2ea1afdbe546b1bf1e4e4d2f712654e89930396
diegoeffe96@gmail.com <diegoeffe96@gmail.com>
2024-11-17 03:20:19 +0100

    some upgrades

--- src/app/components/navigator/navigator.component.ts
+++ src/app/components/navigator/navigator.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output, AfterViewInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { ThemeswitchComponent } from './themeswitch/themeswitch.component';
 
@@ -14,15 +14,11 @@ import { ThemeswitchComponent } from './themeswitch/themeswitch.component';
   templateUrl: './navigator.component.html',
   styleUrls: ['./navigator.component.scss']
 })
-export class NavigatorComponent implements AfterViewInit {
-  @Input() totalSections: number = 0;
+export class NavigatorComponent {
+  @Input() totalSections: number = 8;
   @Input() currentSectionIndex: number = 1;
   @Output() navigateNext = new EventEmitter<void>();
   @Output() navigatePrevious = new EventEmitter<void>();
-
-  ngAfterViewInit() {
-    // Niente da fare qui, rimuoviamo l'observer
-  }
 
   onNext() {
     this.navigateNext.emit();
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChildren, QueryList, AfterViewInit, ChangeDetectorRef, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProjectsComponent } from '../../components/projects/projects.component';
 import { AboutComponent } from '../../components/about/about.component';
@@ -15,69 +15,42 @@ import { ExperiencesComponent } from '../../components/experiences/experiences.c
   standalone: true,
   imports: [
     CommonModule,
-    ProjectsComponent, 
-    AboutComponent, 
-    HeroComponent, 
-    SkillsComponent, 
+    ProjectsComponent,
+    AboutComponent,
+    HeroComponent,
+    SkillsComponent,
     NavigatorComponent,
     EducationComponent,
     StatsComponent,
     ContactMeComponent,
-    ExperiencesComponent
+    ExperiencesComponent,
   ],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements AfterViewInit {
-  @ViewChildren('section') sections!: QueryList<ElementRef>;
+export class HomeComponent {
+  sections = [
+    'hero',
+    'about',
+    'projects',
+    'skills',
+    'education',
+    'experiences',
+    'stats',
+    'contact',
+  ];
+
   currentSectionIndex = 0;
 
-  constructor(private cdr: ChangeDetectorRef) { }
-
-  ngAfterViewInit() {
-    this.updateCurrentSectionIndex();
-  }
-
-  // Listener per l'evento di scroll che aggiorna la sezione corrente
-  @HostListener('window:scroll', [])
-  onScroll() {
-    this.updateCurrentSectionIndex();
-  }
-
-  // Metodo per aggiornare dinamicamente l'indice della sezione attualmente visibile
-  updateCurrentSectionIndex() {
-    const sectionElements = this.sections.toArray();
-    const scrollPosition = window.scrollY + window.innerHeight / 2;
-
-    for (let i = 0; i < sectionElements.length; i++) {
-      const section = sectionElements[i].nativeElement;
-      const sectionTop = section.offsetTop;
-      const sectionHeight = section.offsetHeight;
-
-      if (scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
-        this.currentSectionIndex = i;
-        this.cdr.detectChanges();
-        break;
-      }
-    }
-  }
-
-  navigateNext() {
-    if (this.sections && this.currentSectionIndex < this.sections.length - 1) {
+  navigateNext(): void {
+    if (this.currentSectionIndex < this.sections.length - 1) {
       this.currentSectionIndex++;
-      this.scrollToSection(this.currentSectionIndex);
     }
   }
 
-  navigatePrevious() {
-    if (this.sections && this.currentSectionIndex > 0) {
+  navigatePrevious(): void {
+    if (this.currentSectionIndex > 0) {
       this.currentSectionIndex--;
-      this.scrollToSection(this.currentSectionIndex);
     }
-  }
-
-  scrollToSection(index: number) {
-    const section = this.sections.toArray()[index].nativeElement;
-    section.scrollIntoView({ behavior: 'smooth', block: 'start' });
   }
 }
